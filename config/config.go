abf5a0d82ea429642d24162d348cfc4e41353773
abdisudiatmika <sudiatmika.abdi@gmail.com>
2019-12-09 11:04:09 +0700

    sebuah data sqllite

--- config/config.go
+++ config/config.go
@@ -1,10 +1,8 @@
 package config
 
 import (
-	"fmt"
-
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
@@ -13,41 +11,50 @@ var (
 )
 
 func init() {
-	initDB()
+	InitDB()
 
 }
 
-//Config untuk menyimpan user
-type Config struct {
-	DBUsername string
-	DBPassword string
-	DBPort     string
-	DBHost     string
-	DBName     string
-}
-
-func initDB() {
-	config := Config{
-		DBUsername: "root",
-		DBPassword: "",
-		DBPort:     "3306",
-		DBHost:     "localhost",
-		DBName:     "dbmvc",
-	}
-
-	connectionString :=
-		fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-			config.DBUsername,
-			config.DBPassword,
-			config.DBHost,
-			config.DBPort,
-			config.DBName,
-		)
-
-	var err error
-	db, err := gorm.Open("mysql", connectionString)
+//InitDB fungsi inisialisasi DB
+func InitDB() {
+	db, err := gorm.Open("sqlite3", "./db/crud_go.db")
 	if err != nil {
 		panic(err)
 	}
 	DB = db
 }
+
+//Config untuk menyimpan user
+// type Config struct {
+// 	DBUsername string
+// 	DBPassword string
+// 	DBPort     string
+// 	DBHost     string
+// 	DBName     string
+// }
+
+// func initDB() {
+// 	config := Config{
+// 		DBUsername: "root",
+// 		DBPassword: "",
+// 		DBPort:     "3306",
+// 		DBHost:     "localhost",
+// 		DBName:     "dbmvc",
+// 	}
+
+// 	connectionString :=
+// 		fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+// 			config.DBUsername,
+// 			config.DBPassword,
+// 			config.DBHost,
+// 			config.DBPort,
+// 			config.DBName,
+// 		)
+
+// 	var err error
+// 	db, err := gorm.Open("mysql", connectionString)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	DB = db
+// }
